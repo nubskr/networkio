39c79240e534a80e252aebf9a843ac6c34c3041c
Daksh Rinwan <dakshrinwan2003@gmail.com>
2025-02-13 03:54:30 +0530

    more stuff, some essential stuff remaining, can be optimized a lot further

--- init.go
+++ init.go
@@ -5,11 +5,20 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
+	"log"
 	"net"
 	"reflect"
+	"sync"
+	"time"
 )
 
 /*
+TODO:
+
+when a connection does, we can save their writequeue and readqueue in case the connection is made again later
+
+use better serialization
+
 TODO handshake:
 
 send:
@@ -25,40 +34,101 @@ const WRITE_QUEUE_BUFFER = 100
 const READ_QUEUE_BUFFER = 100
 
 type Message struct {
-	uuid string
-	data string
+	header string
+	uuid   string
+	data   string
 }
 
-func getNewMessage(data string) Message {
+func getNewMessage(data string, header string) Message {
 	return Message{
-		uuid: getUUID(),
-		data: data,
+		header: header,
+		uuid:   getUUID(),
+		data:   data,
 	}
 }
 
 type Connection struct {
-	ClientId        string // this is our id
-	TargetId        string // this is what we are connecting to, just an uuid like ClientId
-	IsInbound       bool   // who initiated this connection
-	WriteQueue      chan Message
-	ReadQueue       chan Message
-	Conn            net.Conn
-	IsUUIDProcessed map[string]bool
+	OurId        string // this is our id
+	PeerId       string // this is what we are connecting to, just an uuid like ClientId
+	IsInbound    bool   // who initiated this connection
+	WriteQueue   chan Message
+	ReadQueue    chan Message
+	Conn         net.Conn
+	sentACKs     sync.Map
+	receivedACKs sync.Map
 }
 
-// private func
+func serializeStruct(s interface{}) (string, error) {
+	val := reflect.ValueOf(s)
+	if val.Kind() != reflect.Struct {
+		return "", errors.New("Not a struct!")
+	}
+	data, err := json.Marshal(s)
+	if err != nil {
+		return "", err
+	}
+	return string(data), nil
+}
+
 func (c *Connection) writeToConnLoop() {
+	conn := c.Conn
 	for {
 		msg := <-c.WriteQueue
-		conn := c.Conn
-		// serialize the
+
+		for {
+			encoder := json.NewEncoder(conn)
+			err := encoder.Encode(msg)
+			if err != nil {
+				log.Print("Error encoding message: %v\n", err)
+				return
+			}
+
+			// wait for ack, TODO: change this nasty shit later
+			time.Sleep(2 * time.Second)
+
+			if val, ok := c.receivedACKs.Load(msg.uuid); ok && val.(bool) {
+				break
+			}
+		}
 	}
+}
+
+func (c *Connection) readFromConnLoop() {
+	conn := c.Conn
 	for {
-		// reads from the queue
+		decoder := json.NewDecoder(conn)
+		var msg Message
+		err := decoder.Decode(&msg)
+		if err != nil {
+			log.Printf("Error decoding message: %v\n", err)
+			return
+		}
 
-		// sends it, waits for ack, if times out, sends it again, waits for ack, and keeps going like that
+		if msg.header == "HANDSHAKE" {
+			ackMsg := getNewMessage(c.OurId, "ACK_HANDSHAKE")
+			c.WriteQueue <- ackMsg
+			return
+		}
 
-		// if ack is received, just moves on with life
+		if msg.header == "ACK" {
+			c.receivedACKs.Store(msg.data, true)
+			continue
+		}
+		if msg.header == "ACK_HANDSHAKE" {
+			// msg.data will be the peerId in this case
+			c.PeerId = msg.data
+			continue
+		}
+
+		ackMsg := getNewMessage(msg.uuid, "ACK")
+		c.WriteQueue <- ackMsg
+
+		if val, ok := c.sentACKs.Load(msg.uuid); ok && val.(bool) {
+			continue
+		}
+
+		c.sentACKs.Store(msg.uuid, true)
+		c.ReadQueue <- msg
 	}
 }
 
@@ -68,47 +138,57 @@ func getUUID() string {
 	if err != nil {
 		return ""
 	}
-	// UUID v4
 	return fmt.Sprintf("%x-%x-%x-%x-%x", b[0:4], b[4:6], b[6:8], b[8:10], b[10:])
 }
 
-// use gob for serialization here ?
 func (c *Connection) WriteToConn(v interface{}) error {
-	// serialises stuff
-	val := reflect.ValueOf(v)
-	if val.Kind() != reflect.Struct {
-		return errors.New("Not a struct!")
-	}
-	data, err := json.Marshal(v)
+	data, err := serializeStruct(v)
 	if err != nil {
 		return err
 	}
-	c.WriteQueue <- getNewMessage(string(data))
+	c.WriteQueue <- getNewMessage(string(data), "PAYLOAD")
 	return nil
 }
 
-func (c *Connection) ReceiveLoop() {
-	// just keeps listening
+func (c *Connection) ReadFromConn() Message {
+	return <-c.ReadQueue
+}
 
-	for {
-		conn := c.Conn
-		// read the data from this conn
+func (c *Connection) doHandshake() {
+	c.WriteQueue <- getNewMessage(c.OurId, "HANDSHAKE")
+	log.Print("handshake initiated")
+}
 
-		// send ack for the data received
-
-		// acks are sent directly, no queue needed for them ? or do we need a queue for them ? yes we do actually, oh wait, we will only be sending one ack at a time, won't we ?
-
+// we go to someone, a -> b
+// note that it is the very starting, so no other communication is being done here
+func InitConnection(addr string, port string, ourId string) Connection {
+	conn, err := net.Dial("tcp", addr+":"+port)
+	if err != nil {
+		log.Printf("Error connecting to %s:%s: %v\n", addr, port, err)
+		return Connection{}
 	}
+
+	// Create new outbound connection
+	newConn := Connection{
+		OurId:        ourId,
+		IsInbound:    false,
+		WriteQueue:   make(chan Message, WRITE_QUEUE_BUFFER),
+		ReadQueue:    make(chan Message, READ_QUEUE_BUFFER),
+		Conn:         conn,
+		sentACKs:     sync.Map{},
+		receivedACKs: sync.Map{},
+	}
+
+	go newConn.readFromConnLoop()
+	go newConn.writeToConnLoop()
+
+	newConn.doHandshake()
+	// TODO: wait here till the handshake is complete, we only return active connections, no async shit
+	return newConn
 }
 
-// connect to some address, us -> them, also do the handshake part
-func InitConnection(c *Connection) error {
-
-}
-
-// TODO: implement the handshake part
 func AcceptConnRequestLoop(c *Connection) {
-	listener, err := net.Listen("tcp", ":8080") // or whatever port you want to use
+	listener, err := net.Listen("tcp", ":8080")
 	if err != nil {
 		return
 	}
@@ -120,18 +200,18 @@ func AcceptConnRequestLoop(c *Connection) {
 			continue
 		}
 
-		// Create new connection for inbound request
+		// someone comes to us, a <- b, they will init the handshake
 		newConn := &Connection{
-			ClientId:        c.ClientId,
-			IsInbound:       true,
-			WriteQueue:      make(chan Message, WRITE_QUEUE_BUFFER),
-			ReadQueue:       make(chan Message, READ_QUEUE_BUFFER),
-			Conn:            conn,
-			IsUUIDProcessed: make(map[string]bool),
+			OurId:        c.OurId,
+			IsInbound:    true,
+			WriteQueue:   make(chan Message, WRITE_QUEUE_BUFFER),
+			ReadQueue:    make(chan Message, READ_QUEUE_BUFFER),
+			Conn:         conn,
+			sentACKs:     sync.Map{},
+			receivedACKs: sync.Map{},
 		}
 
-		// Start read and write routines for new connection
-		go newConn.ReceiveLoop()
+		go newConn.readFromConnLoop()
 		go newConn.writeToConnLoop()
 	}
 }
